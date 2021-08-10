758350dcd8f2dcbe3a5c3791fc38d66838cc7621
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2021-08-10 19:00:21 +0900

    Fix: #1 Home画面実装

---
+++ components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row class="d-flex justify-center">
+      <NuxtLink to="/" class="mr-5 grey--text text-decoration-none text-sm-caption">Home</NuxtLink>
+      <NuxtLink to="/about" class="mr-5 grey--text text-decoration-none text-sm-caption">About</NuxtLink>
+      <NuxtLink to="/works" class="mr-5 grey--text text-decoration-none text-sm-caption">Works</NuxtLink>
+      <NuxtLink to="/contact" class="mr-5 grey--text text-decoration-none text-sm-caption">Contact</NuxtLink>
+    </v-row>
+    <v-row>
+      <v-col class="d-flex justify-center">
+        <v-btn icon href="https://github.com/akiyoshiyamamoto">
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+        <v-btn icon href="https://twitter.com/">
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-col class="text-center grey--text text-sm-caption">
+      {{ new Date().getFullYear() }} - <strong>yaak</strong>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="pb-10">
+    <v-row class="d-flex justify-end">
+      <h1 class="font-italic font-weight-thin text-sm-h1">
+        yaak
+        <br>
+        portfolio
+        <br>
+        site
+      </h1>
+    </v-row>
+    <v-row class="d-flex justify-start">
+      <v-img
+        :src="require('~/assets/images/dog.png')"
+      />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
